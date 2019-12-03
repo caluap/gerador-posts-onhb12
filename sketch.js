46a1ca5139e13f92fb0f67c1c2d6e374d7022cdf
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2019-12-03 15:31:08 -0300

    can change format

--- sketch.js
+++ sketch.js
@@ -8,3 +8,18 @@ function setup() {
 function draw() {
   background(255);
 }
+
+function setSizeFormat(format) {
+  let sizeConfig = {
+    facebook_feed: { w: 1200, h: 630 },
+    twitter_feed: { w: 1024, h: 512 },
+    instagram_feed: { w: 1080, h: 1080 },
+    instagram_stories: { w: 1080, h: 1920 }
+  };
+
+  if (!(format in sizeConfig)) {
+    // a nice default?
+    format = "instagram_feed";
+  }
+  resizeCanvas(sizeConfig[format].w, sizeConfig[format].h);
+}
