971996a07859ace50896ce36430757c56b6ec09a
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2020-02-19 16:31:42 -0300

    draws texts

--- sketch-depoimentos.js
+++ sketch-depoimentos.js
@@ -13,7 +13,7 @@ function setup() {
   spinner = select("#spinner");
   noLoop();
 
-  sliderFsTestimony = createSlider(20, 100, 60, 2);
+  sliderFsTestimony = createSlider(20, 100, 50, 1);
   sliderFsTestimony.changed(updateCanvas);
   let container = select("#slider-fs-testimony");
   container.child(sliderFsTestimony);
@@ -36,11 +36,36 @@ function handleUpload(file) {
   //   });
   // }
 }
+
+function drawText() {
+  textFont(mainFont);
+  textAlign(LEFT, TOP);
+  fill(255);
+
+  let fsMainText = sliderFsTestimony.value();
+
+  let w1 = textSize(fsMainText);
+  textLeading((fsMainText * 4) / 3);
+  text(
+    data.text.testimony,
+    500,
+    margin,
+    width - 500 - margin * 2,
+    height - margin * 2
+  );
+
+  textSize(32);
+  textLeading(50);
+
+  let dep = `${data.text.deponent}, ${data.text.deponentOrigin}`;
+  text(dep, margin, 500, width - 500 - margin * 3, height - margin - 500);
+}
+
 function draw() {
   background(0);
   // drawImage();
   // drawPattern();
   // drawLogo();
-  // drawText();
+  drawText();
   spinner.elt.style.opacity = "0";
 }
